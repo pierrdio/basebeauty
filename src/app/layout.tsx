a7ed1123e35470d145f9eee0d14c681d08cec152
pierrdio <maksimyurt@MacBook-Air-Maksim.local>
2025-12-22 16:57:33 +0300

    ya.metriki

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { headers } from "next/headers";
 import { Toaster } from "@/components/ui/sonner";
+import YandexMetrika from "@/components/YandexMetrika";
 
 
 const geistSans = Geist({
@@ -50,6 +51,7 @@ export default async function RootLayout({
         ) : (
           <main className="grow">
             {children}
+            <YandexMetrika />
           </main>
         )}
         <Toaster />
